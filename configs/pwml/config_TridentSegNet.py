43601fd80a9bd8851ac51889bb20e95769c43f5a
Yalong Liu <jamesliu2248@gmail.com>
2020-05-26 23:17:09 +0800

    Modify loss name

--- configs/pwml/config_TridentSegNet.py
+++ configs/pwml/config_TridentSegNet.py
@@ -4,7 +4,7 @@ from frames.config import Config
 from imgaug import augmenters as iaa
 
 from keras.optimizers import Adam, SGD
-from frames.metrics import mean_iou, dice_coef_metrics, self_adaptive_balance_loss, focal_loss
+from frames.metrics import mean_iou, dice_coef_metrics, self_balancing_focal_loss, focal_loss
 
 
 ############################################################
@@ -13,8 +13,9 @@ from frames.metrics import mean_iou, dice_coef_metrics, self_adaptive_balance_lo
 class ModelConfig(Config):
     # Modify to your own path
     DATA_DIR = './test_data'  # choose input data
+    # DATA_DIR = 'F:/datasets/PWML/cut2skull/8b'  # choose input data
     WEIGHTS_PATH = ''  # Weights to load
-    # WEIGHTS_PATH = "F:\datasets\logs/tridentsegnet_pwml_3slice_random_98765_20190929T2211/0050.h5"
+    # WEIGHTS_PATH = "F:/datasets/weights/tridentsegnet_pwml_cut2skull_8b_98765_20190922T1712_0050_pred_0.7.h5"
 
     ###################################################################################################################
     #                         Modifications to the following section are NOT recommended!                             #
@@ -32,7 +33,7 @@ class ModelConfig(Config):
     IMAGES_PER_GPU = 8  # Adjust depending on your GPU memory. Batch size is 4 (GPUs * images/GPU).
     DATA_RATIO = [0.8, 0.2]  # 5-fold
     NUM_CLASSES = 2
-    CARDINALITY = 32
+    CARDINALITY = 64
     MEAN_PIXEL = np.array([56.49, 56.49, 56.49])
     PATCH_SIZE = [128, 128]
 
@@ -45,13 +46,13 @@ class ModelConfig(Config):
 
     OPTIMIZER = Adam
     # OPTIMIZER = SGD
-    LOSS_FUNCTION = [self_adaptive_balance_loss]
+    LOSS_FUNCTION = [self_balancing_focal_loss]
     # LOSS_FUNCTION = [focal_loss]
     METRICS = [mean_iou, dice_coef_metrics]
 
     SAVE_BEST = False
-    SAVE_EPOCHES = 10
-    PATIENCE = 10
+    SAVE_EPOCHES = 1
+    PATIENCE = 1
 
     # Number of training and validation steps per epoch
     STEPS_PER_EPOCH = 50
@@ -63,8 +64,8 @@ class ModelConfig(Config):
     IMAGE_PADDING = True
     IMAGE_CHANNEL_COUNT = 3
 
-    augmentation = iaa.Sequential(iaa.Crop(percent=(0, [0, 0.05], 0, [0, 0.05]), keep_size=True), iaa.Fliplr(
-        0.5), iaa.Affine(rotate=(-10, 10)), iaa.Multiply((0.8, 1.2)))
+    augmentation = iaa.Sequential(children=(iaa.Crop(percent=(0, [0, 0.05], 0, [0, 0.05]), keep_size=True), iaa.Fliplr(
+        0.5), iaa.Affine(rotate=(-10, 10)), iaa.Multiply((0.8, 1.2))))
 
     # The name of model to save
     NAME = MODLE_NAME + '_' + LOAD_DATA_NAME + '_' + str(RANDOM_SEED) + '_'
